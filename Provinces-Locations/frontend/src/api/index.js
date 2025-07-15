e98cc72ef2221a7b370f7d144e12c2ace2ab535e
keylet <alexpazos88@gmail.com>
2025-07-15 13:46:56 -0500

    changes ports - frontend

--- Provinces-Locations/frontend/src/api/index.js
+++ Provinces-Locations/frontend/src/api/index.js
@@ -1,10 +1,13 @@
 import axios from 'axios';
 
-const PROV_GET   = 'http://localhost:4004';
-const PROV_DEL   = 'http://localhost:4005';
-const LOC_GET    = 'http://localhost:4002';
-const LOC_CREATE = 'http://localhost:4001';
-const LOC_DEL    = 'http://localhost:4003';
+// IP pública de tu instancia EC2 en AWS
+const IP = 'http://52.7.131.155';
+
+const PROV_GET   = `${IP}:4004`;
+const PROV_DEL   = `${IP}:4005`;
+const LOC_GET    = `${IP}:4002`;
+const LOC_CREATE = `${IP}:4001`;
+const LOC_DEL    = `${IP}:4003`;
 
 export const getProvinces   = () => axios.get(`${PROV_GET}/provinces/`);
 export const deleteProvince = id => axios.delete(`${PROV_DEL}/provinces/${id}/`);
